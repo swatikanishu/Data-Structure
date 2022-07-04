e1ffa5a4812a11c3e2e15c15d606263469540d00
swatikanishu <nishuswatika@gmail.com>
2022-07-04 14:29:42 +0530

    Added: Datastructure Program   is added

---
+++ src/arrayanagram/Main.java
@@ -0,0 +1,11 @@
+package arrayanagram;
+import java.util.Arrays;
+public class Main {
+
+        public static void main(String[] args) {
+            PrimeAnagram primeAnagram = new PrimeAnagram();
+            primeAnagram.anagramNo();
+            primeAnagram.show();
+        }
+    }
+
